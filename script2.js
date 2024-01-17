a0101ebbf4d9449f75e84f421a58fb7a70b3b8d6
Raktim Banerjee <raktimbanerjee05@gmail.com>
2024-01-17 18:46:48 +0530

    Learning and saving Progress

---
+++ script2.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static('./Public'));
+app.get('/', function (req, res) {
+  res.render("index");
+})
+app.get('/contact', function (req, res) {
+  res.render("contact");
+})
+app.get('/profile', function (req, res) {
+  res.send('Hellooo to all');
+})
+app.get('/profile/:username', function (req, res) {
+  res.send(`Heloo to ${req.params.username}`);
+})
+app.get('/cynthia', function (req, res) {
+  res.render("cynthia");
+})
+app.get('/error', function (req, res) {
+  throw Error()
+})
+app.use(function errorHandler (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err)
+  }
+  res.status(500)
+  res.render('error', { error: err })
+})
+app.listen(3000)
